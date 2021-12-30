74b3d72c79489d429885aee838ccf8d667d36473
jl6078 <95923788+jl6078@users.noreply.github.com>
2021-12-30 14:43:37 -0500

    Update SorS.py

--- SorS.py
+++ SorS.py
@@ -1,8 +1,7 @@
 import pandas as pd
 df = pd.read_csv('CSVs/SorS.csv')
-output_df = df[df['Name'] == params["name"]]
-output_df.to_csv("output/SorS stats.csv")
-
 params = { 
     "name" : "Gil Dexter"
 }
+output_df = df[df['Name'] == params["name"]]
+output_df.to_csv("output/SorS stats.csv")
