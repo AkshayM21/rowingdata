235a3c51c29b11eb5f0b528076fe6a52b11799cb
jl6078 <95923788+jl6078@users.noreply.github.com>
2022-01-03 16:58:01 -0500

    code to process dictionary from rower csv file

---
+++ online-coaching/api/rowerCSVs.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+df = pd.DataFrame({'date':[], 'workout_type':[], 'avg_power':[], 'avg_stroke_rate':[], 'avg_stroke_length':[], 'avg_pulse':[], 'avg_energy_per_stroke':[], 'avg_500m_time':[], 'leg_2_avg_power':[], 'leg_2_avg_pulse':[], 'leg_3_avg_power':[], 'leg_3_avg_pulse':[], 'decoupling_rate':[]})
+rowers = pd.read_csv('CSVs/rowers.csv')
+#column_order = ['date', 'workout_type', 'avg_power', 'avg_stroke_rate', 'avg_stroke_length', 'avg_pulse', 'avg_energy_per_stroke', 'avg_500m_time', 'leg_2_avg_power', 'leg_2_avg_pulse', 'leg_3_avg_power', 'leg_3_avg_pulse', 'decoupling_rate']
+uni_list = []
+email_list = rowers['Email']
+name_list = rowers['Name']
+
+for rower in email_list:
+    uni_list.append(rower.split('@')[0])
+
+df2 = pd.DataFrame({'uni': uni_list, 'name': name_list})
+column_order = ['uni', 'name']
+dictionary = dict(df2[column_order].values)
+
+#for element in list:
+#    df[column_order].to_csv('output/' + element + '.csv')
+#df[column_order].to_csv('output/ykv2002.csv')
