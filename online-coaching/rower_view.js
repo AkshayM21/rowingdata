3f66d7b2e9ebc234be07b13d664a136f2a206f08
jl6078 <95923788+jl6078@users.noreply.github.com>
2022-01-07 20:48:48 -0500

    Update rower_view.js

--- online-coaching/rower_view.js
+++ online-coaching/rower_view.js
@@ -3,15 +3,80 @@ import ReactDOM from 'react-dom'
 import {Base} from './index.js'
 
 class Rower extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            uni: 'N/A', 
+            date: 'N/A',
+            description: 'N/A',
+            time_on: 'N/A',
+            workout_type: 'N/A',
+            rpe: 'N/A',
+            file:'N/A'
+        };
+        this.handleChange=this.handleChange.bind(this);
+        this.handleSelectChange = this.selectChange.bind(this);
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+    }
+
+    handleChange(e) {
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({[name]: value})
+    }
+
+    handleSelectChange(e) {
+        this.setState({workout_type: e.target.value});
+    }
     render() {
         return(
             <div>
                 <Base/>
                 <h1>Submit Workout Information</h1>
-                Submission forms for params/file
+                <form onSubmit={this.handleSubmit} method="POST">
+                    <label>
+                        UNI:
+                        <input type="text" name="uni" value={this.state.uni} onChange={this.handleChange}/>
+                    </label>
+                    <label>
+                        Date:
+                        <input type="text" name="date" value={this.state.date} onChange={this.handleChange}/>
+                    </label>
+                    <label>
+                        Workout Description (6x8min erg):
+                        <input type="text" name="description" value={this.state.description} onChange={this.handleChange}/>
+                    </label>   
+                    <label>
+                        Split Time (6x8 input 8):
+                        <input type="text" name="time_on" value={this.state.time_on} onChange={this.handleChange}/>
+                    </label>                
+                    <label>
+                        Workout Type:
+                        <select value={this.state.value} onChange={this.handleSelectChange} name="workout_type">
+                            <option value="decoupling">3x19 Decoupling Workout</option>
+                            <option value="rp3">Other RP3 workout</option>
+                            <option value="hr_data">Garmin HR Data</option>
+                        </select>
+                    </label>      
+                    <label>
+                        RPE (1-10):
+                        <input type="text" name="rpe" value={this.state.rpe} onChange={this.handleChange}/>
+                    </label>
+                    <label>
+                        File:
+                        <input type="file" id="input"/>
+                    </label>
+                    <button type="submit">Submit</ button>
+                </form>
             </div>
         )
     }
 }
 
-ReactDOM.render(<Rower />, document.getElementById('app'))
+//Params are stored in compoment state, file can be obtained with id
+ReactDOM.render(<Rower />, document.getElementById('rower_view'))
