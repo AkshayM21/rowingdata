b0d68613379e93fb77c3f5a221d7ee8df6a0de61
jl6078 <95923788+jl6078@users.noreply.github.com>
2022-01-13 00:46:23 -0500

    Update coach_view.js

--- online-coaching/src/coach_view.js
+++ online-coaching/src/coach_view.js
@@ -3,50 +3,11 @@ import { Navigate } from "react-router-dom";
 import { UserContext } from "./providers/UserProvider"
 import {Box, Drawer, Tab, Card, CardContent, Typography, Grid, Toolbar, Divider, MenuList, MenuItem, ListItemText}  from '@mui/material';
 import { TabContext, TabList, TabPanel } from '@mui/lab';
+import { DataGrid } from '@mui/x-data-grid';
+import { RowerMenu } from "./RowerMenu";
 //import CardActions from '@mui/material/CardActions';
 
 const drawerWidth = 240;
-// RowerMenu passes uni up to Page
-class RowerMenu extends React.Component {
-    constructor(props) {
-        super(props);
-        // JSON of rower names, format {'jl6078': 'Jonathan Liu', 'uni2':'Name2'}
-        this.state={rowers: {'jl6078': 'Jonathan Liu', 'uni2':'Name2'}};
-    }
-
-    handleChange(e) {
-        const uni=e.target.value;
-        this.props.onChange(uni, this.state.rowers[uni]);
-    }
-
-    render() {      
-        return (
-            <Box>
-                <Drawer
-                variant="permanent"
-                ModalProps={{
-                    keepMounted: true,
-                }}
-                sx={{
-                    display: { xs: 'block', sm: 'none' },
-                    '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-                }}
-                >
-                    <Toolbar />
-                    <Divider />
-                    <MenuList>
-                        {Object.entries(this.state.rowers).map(([uni, name]) => (
-                            <MenuItem value={uni} onClick={() => (this.handleChange)}>
-                                <ListItemText primary={name} />
-                            </MenuItem>
-                        ))}
-                    </MenuList>
-                    <Divider />
-                </Drawer>
-            </Box>
-        );
-    }
-}
 
 function RowerCard(props){
     // Workout json passed down from deck
@@ -54,13 +15,13 @@ function RowerCard(props){
         <Card style={{ width: '16rem' }}>
             <CardContent>
                 <Typography sx={{ fontSize: 14 }} color="text.secondary">
-                    {this.props.workout['date']}
+                    {props.workout['date']}
                 </Typography>
                 <Typography>
-                    Workout:{this.props.workout['workout_description']}
+                    Workout:{props.workout['workout_description']}
                 </Typography>
                 <Typography sx={{ fontSize: 14 }}>
-                    RPE:{this.props.workout['rpe']}
+                    RPE:{props.workout['rpe']}
                 </Typography>
             </CardContent>
             {/*<CardActions>
@@ -90,19 +51,20 @@ function Workouts(props) {
 
 
 function SorS(props) {
-    const[results, setResults] = useState([{'date': '01/15/21', 'rpe': '10', 'workout_description':'3x19'}, {'date': '01/18/21', 'rpe': '15', 'workout_description':'3x19'}]);  
+    const[results, setResults] = useState([{'id': '01/15/21', 'rpe': '10', 'workout_description':'3x19'}, {'id': '01/18/21', 'rpe': '15', 'workout_description':'3x19'}]);  
     // Results is array of JSON objects
     // Need GET method for json, use props.uni to call   
+    const rows= results;
+    const columns= [
+        { field: 'id', headerName: 'Date', width: 150 },
+        { field: 'rpe', headerName: 'RPE', width: 150 },
+        { field: 'workout_description', headerName: 'Workout: ', width: 150 }
+    ]
+
     return (
-        <Box>
-            {results.map((row,i) => (
-                <tr key={i}>
-                    {row.map((cell) => (
-                        <td>{cell}</td>
-                    ))}
-                </tr>
-            ))}
-        </Box>
+        <div style={{ height: 300, width: '100%' }}>
+            <DataGrid rows={rows} columns={columns} />
+        </div>
     );
 }
 
@@ -115,19 +77,19 @@ function RowerTabs(props) {
     };
 
     return (
-        <Box sx={{ width: '100%', typography: 'body1' }}>
+        <Box sx={{ flexGrow: 1, p: 3, ml: { sm: `${drawerWidth}px` }, width: { sm: `calc(100% - ${drawerWidth}px)` } }}>
             <TabContext value={value}>
                 <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                    <TabList value={value} onChange={handleChange}>
+                    <TabList onChange={handleChange}>
                         <Tab label="Workouts" value="1"/>
                         <Tab label="SorS" value="2"/>
                     </TabList>
                 </Box>
                 <TabPanel value="1">
-                    <Workouts uni={this.props.uni} />
+                    <Workouts uni={props.uni} />
                 </TabPanel>
                 <TabPanel value="2">
-                    <SorS uni={this.props.uni}/>
+                    <SorS uni={props.uni}/>
                 </TabPanel>
             </TabContext>            
         </Box>  
@@ -136,7 +98,7 @@ function RowerTabs(props) {
 }
 
 function Page(props) {
-    const user = useContext(UserContext)
+    /*const user = useContext(UserContext)
     const [redirect, setredirect] = useState(null)
 
     useEffect(() => {
@@ -147,21 +109,22 @@ function Page(props) {
 
     if (redirect) {
         return <Navigate to={redirect}/>
-    }
+    }*/
         
     const[uni, setUni] = useState('');
     const[name, setName] = useState('');
 
-    const handleChange= (newUni, newName) => {
+    const changeName = (newUni, newName) => {
         setUni(newUni);
         setName(newName);
     }
 
     return(
         <div>
-            <RowerMenu onChange={handleChange} />
-            <h1>{name}</h1>
+            <RowerMenu onClick={() => changeName} />
+            {uni}
             <RowerTabs uni={uni} />
+            {uni}
         </div>
     )
 }
