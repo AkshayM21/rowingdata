0e6618da38c6f2045534d43745a3ba4deefe3b50
jl6078 <95923788+jl6078@users.noreply.github.com>
2022-02-06 16:55:47 -0500

    Fully updated force profile with right code

--- online-coaching/src/force_profile.js
+++ online-coaching/src/force_profile.js
@@ -1,30 +1,19 @@
 import React, { useState, useEffect } from 'react'
-import {DialogTitle, Dialog, Button, Typography} from '@mui/material';
+import Dialog from '@mui/material/Dialog'
 import PropTypes from 'prop-types';
+import Button from '@mui/material/Button';
 import { Base64 } from 'js-base64';
 
 function ForceDialog(props) {
     const { onBackdropClick, open } = props;
-    //const [image, setImage] = useState();
+    const [image, setImage] = useState();
 
-    /*const decodeBase64 = (base64data) => {
-        let base64ToString = Buffer.from(base64data, "base64").toString()
-        setImage(base64ToString)
-    }
-    /*useEffect(() => {
-        setImage(JSON.stringify(Base64.atob(props.img)));
-    })*/
-    //image=decodeBase64(`${props.img}`);
     // Calls onClose- sets open to false
-    
     return(
         // onClose is built into Dialog such that clicking outside calls handleClose, which calls onBackdropClick prop
         <Dialog onBackdropClick={onBackdropClick} open={open} >
             <img src={`data:image/png;base64,${props.img}`}/>
-            <Typography sx= {{fontWeight: 'bold', mt: '5%'}} align={'center'}>
-                IMSE: {props.imse}
-            </Typography>
-            <img src={`data:image/png;base64,${props.variance}`}/>
+            {/*import and display image*/}
         </Dialog>
     ); 
 }
@@ -37,6 +26,11 @@ ForceDialog.propTypes = {
 export default function ForceProfile(props) {
 
     const [open, setOpen] = useState(false);
+
+    /*useEffect(() => {
+        fetch(`forceprofile?workout_id=${props.workout_id}`)
+        .then()
+    })*/
 
     // handleOpen called when button is clicked
     const handleOpen = () => {
@@ -57,8 +51,6 @@ export default function ForceProfile(props) {
                 open={open}
                 onBackdropClick={handleClose}
                 img={props.img}
-                variance={props.variance}
-                imse={props.imse}
             />
         </div>
     );
