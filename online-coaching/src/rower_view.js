0010ac0b501e8aead133fb9c9da2273376799981
jl6078 <95923788+jl6078@users.noreply.github.com>
2022-02-06 09:48:08 -0500

    Minor changes

--- online-coaching/src/rower_view.js
+++ online-coaching/src/rower_view.js
@@ -3,7 +3,6 @@ import { Navigate } from "react-router-dom";
 import {UserContext} from "./providers/UserProvider"
 import SubmissionForm from "./SubmissionForm";
 
-
 export default function Rower(){
     const user = useContext(UserContext)
     const [redirect, setredirect] = useState(null)
@@ -24,11 +23,7 @@ export default function Rower(){
             <div>
                 <h1>Submit Workout Information</h1>
                 <SubmissionForm/>
+                <button>Settings</button>
             </div>
      )
 }
-
-
-
-// //Params are stored in compoment state, file can be obtained with id
-// ReactDOM.render(<Rower />, document.getElementById('rower_view'))
