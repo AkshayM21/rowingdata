69f9bd311f78fdfbc13c251cdd0995a56ac46116
jl6078 <95923788+jl6078@users.noreply.github.com>
2022-02-06 16:40:15 -0500

    Changed dialog display

--- online-coaching/src/force_profile.js
+++ online-coaching/src/force_profile.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from 'react'
-import Dialog from '@mui/material/Dialog'
-import DialogTitle from '@mui/material/DialogTitle';
+import {DialogTitle, Dialog, Button, Typography} from '@mui/material';
 import PropTypes from 'prop-types';
-import Button from '@mui/material/Button';
 import { Base64 } from 'js-base64';
 
 function ForceDialog(props) {
     const { onBackdropClick, open } = props;
-    const [image, setImage] = useState();
+    //const [image, setImage] = useState();
 
     /*const decodeBase64 = (base64data) => {
         let base64ToString = Buffer.from(base64data, "base64").toString()
@@ -18,11 +16,15 @@ function ForceDialog(props) {
     })*/
     //image=decodeBase64(`${props.img}`);
     // Calls onClose- sets open to false
+    
     return(
         // onClose is built into Dialog such that clicking outside calls handleClose, which calls onBackdropClick prop
         <Dialog onBackdropClick={onBackdropClick} open={open} >
-            <DialogTitle>Advanced</DialogTitle>
             <img src={`data:image/png;base64,${props.img}`}/>
+            <Typography sx= {{fontWeight: 'bold', mt: '5%'}} align={'center'}>
+                IMSE: {props.imse}
+            </Typography>
+            <img src={`data:image/png;base64,${props.variance}`}/>
         </Dialog>
     ); 
 }
@@ -35,11 +37,6 @@ ForceDialog.propTypes = {
 export default function ForceProfile(props) {
 
     const [open, setOpen] = useState(false);
-
-    /*useEffect(() => {
-        fetch(`forceprofile?workout_id=${props.workout_id}`)
-        .then()
-    })*/
 
     // handleOpen called when button is clicked
     const handleOpen = () => {
@@ -60,6 +57,8 @@ export default function ForceProfile(props) {
                 open={open}
                 onBackdropClick={handleClose}
                 img={props.img}
+                variance={props.variance}
+                imse={props.imse}
             />
         </div>
     );
